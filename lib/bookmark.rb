3cd09c9bd63005756308c44eabc912b64be6f3d6
LucyMHall <lucymhall1995@gmail.com>
2019-06-21 15:28:32 +0100

    add line which check valid url, needs to be uncommented to run

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -24,6 +24,7 @@ class Bookmark
   end
 
   def self.create(url:, title:)
+    # return false unless is_url?(url) #this will stop the creation of a bookmark if not valid url
     conn = if ENV['ENVIRONMENT'] == 'test'
              PG.connect(dbname: 'bookmark_manager_test')
            else
